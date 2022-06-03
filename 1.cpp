9371af58959def3b3a516f7cfd31b391a4b67fa4
Abishek-G <105722358+Abishek-G@users.noreply.github.com>
2022-06-03 11:39:06 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -6,11 +6,11 @@ Find the sum of all the multiples of 3 or 5 below 1000.*/
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int sum=0;
-    for(int i=1;i<1000;i++){
-        if(i%3==0||i%5==0){
-            sum=sum+i;
+    int sumOfMultiples = 0;
+    for(int itr = 1; itr < 1000; itr++){
+        if(i%3 == 0||itr%5 == 0){
+            sumOfMultiples=sumOfMultiples+itr;
         }
     }
-    cout<<sum;
+    cout<<sumOfMultiples;
 }
